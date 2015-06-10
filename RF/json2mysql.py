6d3a2c0d43bab99c5fa3873c94002d160553793b
Dmitry Zinoviev <dzinoviev@gmail.com>
2015-06-09 21:52:29 -0400

    More RF files

---
+++ RF/json2mysql.py
@@ -0,0 +1,25 @@
+import glob, json, pymysql
+
+conn = pymysql.connect (host = 'localhost', port = 3306, user = 'cs310', passwd = 'badpassw0rd', db = 'cs310', charset = "utf8")
+cur = conn.cursor ()
+
+for f in glob.glob ('2008-KZ-*.json'):
+    with open (f) as infile:
+        data = json.load (infile)
+        for key in data["entities"]:
+            e_type = data["entities"][key]["type"]
+            e_name = data["entities"][key]["name"]
+            cur.execute ("INSERT IGNORE INTO rf_entities VALUES (NULL, NULL, %s, %s, %s)",
+                         (key, e_type, e_name))
+            if "category" in data["entities"][key].keys ():
+                e_id = cur.lastrowid
+                for cat_id in data["entities"][key]["category"]:
+                    cur.execute ("INSERT INTO rf_entity_categories VALUES (NULL, NULL, %s, %s)",
+                                 (e_id, cat_id))
+        conn.commit ()
+ 
+cur.execute ("SELECT type,COUNT(*) AS cnt FROM rf_entities GROUP BY type ORDER BY cnt DESC LIMIT 10")
+
+print ("#Type|Count")
+for (t, count) in cur.fetchall ():
+    print (t, count, sep = '|')
